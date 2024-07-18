7441fa7c4ef306d7c5635eb5bf8fcf7b1d95a959
shahirsyed <shahirsyed143@gmail.com>
2024-07-18 18:03:20 +0530

    Update insertionSort.java

--- src/sorts/insertionSort.java
+++ src/sorts/insertionSort.java
@@ -2,7 +2,7 @@
     Insertion sort: Time complexity is 0(n²) .
  * it works similar to th e sorting of playing cards if we assume that 
  * first card is already sorted in the card game then we select unsortedcard
- * if wwe selected unsorted card is greater than the first card or previous card ,it will placed at the right side. 
+ * if we selected unsorted card is greater than the first card or previous card ,it will placed at the right side. 
  * otherwise it will be placed at the left side 
  * similarly all unwanted cards are taken and put in the exact places
  
